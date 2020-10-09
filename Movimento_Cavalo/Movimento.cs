c504be18e35d0273358463b63e00f0ab58cac669
Gabriel <farmagiancotti10@gmail.com>
2020-10-08 22:32:38 -0300

    Projeto_Final

--- Movimento_Cavalo/Movimento.cs
+++ Movimento_Cavalo/Movimento.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Movimento_Cavalo
 {
-    class Movimento
+    public class Movimento
     {
         public int x, y;
         public int dis;
@@ -13,6 +13,54 @@ namespace Movimento_Cavalo
             this.x = x;
             this.y = y;
             this.dis = dis;
+        }
+
+        static bool VerificarDentro(int x, int y, int N)
+        {
+            if (x >= 1 && x <= N && y >= 1 && y <= N)
+                return true;
+            return false;
+        }
+
+        public int MovimentoMinimoChegada(int[] PosIn, int[] PosFim, int Tam)
+        {
+            int[] movlinha = { -2, -1, 1, 2, -2, -1, 1, 2 };
+            int[] movcoluna = { -1, -2, -2, -1, 1, 2, 2, 1 };
+
+            Queue<Movimento> fila = new Queue<Movimento>();
+            fila.Enqueue(new Movimento(PosIn[0], PosIn[1], 0));
+
+            Movimento t;
+            int x, y;
+            bool[,] visit = new bool[Tam + 1, Tam + 1];
+
+            for (int i = 1; i <= Tam; i++)
+                for (int j = 1; j <= Tam; j++)
+                    visit[i, j] = false;
+
+            visit[PosIn[0], PosIn[1]] = true;
+
+            while (fila.Count != 0)
+            {
+                t = fila.Peek();
+                fila.Dequeue();
+
+                if (t.x == PosFim[0] && t.y == PosFim[1])
+                    return t.dis;
+
+                for (int i = 0; i < 8; i++)
+                {
+                    x = t.x + movlinha[i];
+                    y = t.y + movcoluna[i];
+
+                    if (VerificarDentro(x, y, Tam) && !visit[x, y])
+                    {
+                        visit[x, y] = true;
+                        fila.Enqueue(new Movimento(x, y, t.dis + 1));
+                    }
+                }
+            }
+            return int.MaxValue;
         }
     }
 }
--- Movimento_Cavalo/Program.cs
+++ Movimento_Cavalo/Program.cs
@@ -4,58 +4,10 @@ using System.Collections.Generic;
 namespace Movimento_Cavalo
 {
     class Program
-    {
-        static bool VerificarDentro(int x, int y, int N)
-        {
-            if (x >= 1 && x <= N && y >= 1 && y <= N)
-                return true;
-            return false;
-        }
-
-        static int MovimentoMinimoChegada(int[] PosIn, int[] PosFim, int Tam)
-        {
-            int[] movlinha = { -2, -1, 1, 2, -2, -1, 1, 2 };
-            int[] movcoluna = { -1, -2, -2, -1, 1, 2, 2, 1 };
-
-            Queue<Movimento> fila = new Queue<Movimento>();
-            fila.Enqueue(new Movimento(PosIn[0], PosIn[1], 0));
-
-            Movimento t;
-            int x, y;
-            bool[,] visit = new bool[Tam + 1, Tam + 1];
-
-            for (int i = 1; i <= Tam; i++)
-                for (int j = 1; j <= Tam; j++)
-                    visit[i, j] = false;
-
-            visit[PosIn[0], PosIn[1]] = true;
-
-            while (fila.Count != 0)
-            {
-                t = fila.Peek();
-                fila.Dequeue();
-
-                if (t.x == PosFim[0] && t.y == PosFim[1])
-                    return t.dis;
-
-                for (int i = 0; i < 8; i++)
-                {
-                    x = t.x + movlinha[i];
-                    y = t.y + movcoluna[i];
-
-                    if (VerificarDentro(x, y, Tam) && !visit[x, y])
-                    {
-                        visit[x, y] = true;
-                        fila.Enqueue(new Movimento(x, y, t.dis + 1));
-
-                    }
-                }
-            }
-            return int.MaxValue;
-        }
-
+    {       
         public static void Main(String[] args)
         {
+            Movimento MyMovimento = new Movimento(0, 0, 0);
             int tamanho_tabuleiro = 8, movlinhain, movcolunain, movlinhafim, movcolunafim;
 
             Console.WriteLine("Digite o movimento inicial do cavalo(Linha): ");
@@ -106,7 +58,7 @@ namespace Movimento_Cavalo
             int[] PosFim = { movlinhafim, movcolunafim };
 
             Console.Write("A quantidade de movimentos é: ");
-            Console.Write(MovimentoMinimoChegada(PosInicio, PosFim, tamanho_tabuleiro));
+            Console.Write(MyMovimento.MovimentoMinimoChegada(PosInicio, PosFim, tamanho_tabuleiro));
 
             Console.ReadKey();
         }
