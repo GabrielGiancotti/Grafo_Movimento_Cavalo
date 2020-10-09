e6a5e1b15991786fcc357761cdd0c7680d150a5f
Gabriel <farmagiancotti10@gmail.com>
2020-10-08 21:15:28 -0300

    Projeto_Final

--- Movimento_Cavalo/Program.cs
+++ Movimento_Cavalo/Program.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 
 class Movimento_Cavalo
 {
-
-    // Classe para armazenar os dados
-    public class cell
+    public class movimento
     {
         public int x, y;
         public int dis;
-
-        public cell(int x, int y, int dis)
+        public movimento(int x, int y, int dis)
         {
             this.x = x;
             this.y = y;
@@ -18,8 +15,6 @@ class Movimento_Cavalo
         }
     }
 
-    // Método utilizado retornando verdadeiro
-    // se (x, y) encontra-se no tabuleiro 
     static bool VerificarDentro(int x, int y, int N)
     {
         if (x >= 1 && x <= N && y >= 1 && y <= N)
@@ -27,23 +22,17 @@ class Movimento_Cavalo
             return false;
     }
 
-    // Método de retorno do degrau minimo
-    // Procurar a posição de destino
     static int MovimentoMinimoChegada(int[] PosIn,
                                     int[] PosFim, int Tam)
     {
-        // direção de x e y, onde o cavalo possa se mover 
-        int[] dx = { -2, -1, 1, 2, -2, -1, 1, 2 };
-        int[] dy = { -1, -2, -2, -1, 1, 2, 2, 1 };
+        int[] movlinha = { -2, -1, 1, 2, -2, -1, 1, 2 };
+        int[] movcoluna = { -1, -2, -2, -1, 1, 2, 2, 1 };
 
-        //fila para armazenar as posições do cavalo no tabuleiro 
-        Queue<cell> q = new Queue<cell>();
-
-        // Inicia a posição do cavaleiro com zero.
-        q.Enqueue(new cell(PosIn[0],
+        Queue<movimento> fila = new Queue<movimento>();
+        fila.Enqueue(new movimento(PosIn[0],
                            PosIn[1], 0));
 
-        cell t;
+        movimento t;
         int x, y;
         bool[,] visit = new bool[Tam + 1, Tam + 1];
 
@@ -51,32 +40,25 @@ class Movimento_Cavalo
             for (int j = 1; j <= Tam; j++)
                 visit[i, j] = false;
 
-            // Visitando posição inicial 
             visit[PosIn[0], PosIn[1]] = true;
 
-
-            // loop até nós tivermos um elemento na fila 
-            while (q.Count != 0)
+            while (fila.Count != 0)
             {
-                t = q.Peek();
-                q.Dequeue();
+                t = fila.Peek();
+                fila.Dequeue();
 
-                // Se a posição de inicio é igual a posição final
-                // retornando a distância
                 if (t.x == PosFim[0] && t.y == PosFim[1])
                     return t.dis;
 
-                // loop para todas as posições do tabuleiro
                 for (int i = 0; i < 8; i++)
                 {
-                    x = t.x + dx[i];
-                    y = t.y + dy[i];
+                    x = t.x + movlinha[i];
+                    y = t.y + movcoluna[i];
 
-                    // se o estado alcançavel não for visitado dentro do tabuleiro, ele aguarda na fila de espera.
                     if (VerificarDentro(x, y, Tam) && !visit[x, y])
                     {
                         visit[x, y] = true;
-                        q.Enqueue(new cell(x, y, t.dis + 1));
+                        fila.Enqueue(new movimento(x, y, t.dis + 1));
                     }
                 }
             }
@@ -96,7 +78,7 @@ class Movimento_Cavalo
         else
         {
             Console.WriteLine("Digite um número válido!!");
-            Environment.Exit(10);
+            Environment.Exit(0);
         }
         Console.WriteLine("Digite o movimento inicial do cavalo(Coluna): ");
         movcolunain = int.Parse(Console.ReadLine());
@@ -107,7 +89,7 @@ class Movimento_Cavalo
         else
         {
             Console.WriteLine("Digite um número válido!!");
-            Environment.Exit(10);
+            Environment.Exit(0);
         }
         Console.WriteLine("Digite o movimento final do cavalo(Linha): ");
         movlinhafim = int.Parse(Console.ReadLine());
@@ -118,7 +100,7 @@ class Movimento_Cavalo
         else
         {
             Console.WriteLine("Digite um número válido!!");
-            Environment.Exit(10);
+            Environment.Exit(0);
         }
         Console.WriteLine("Digite o movimento final do cavalo(Coluna): ");
         movcolunafim = int.Parse(Console.ReadLine());
@@ -129,14 +111,13 @@ class Movimento_Cavalo
         else
         {
             Console.WriteLine("Digite um número válido!!");
-            Environment.Exit(10);
+            Environment.Exit(0);
         }
         int[] PosInicio = { movlinhain, movcolunain };
         int[] PosFim = { movlinhafim, movcolunafim };
         
         Console.WriteLine(MovimentoMinimoChegada(PosInicio, PosFim, tamanho_tabuleiro));
         
-
         Console.ReadKey();
     }
 }
